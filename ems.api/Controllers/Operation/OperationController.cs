c9d52434bd63955c2ffee0773cd10adbbd101d0b
deepesh gupta <qtechnologies09@outlook.com>
2025-07-26 09:47:21 +0530

    change

--- ems.api/Controllers/Operation/OperationController.cs
+++ ems.api/Controllers/Operation/OperationController.cs
@@ -147,4 +147,6 @@ namespace ems.api.Controllers.Operation
 
     }
 
+
+
 }
--- ems.api/Program.cs
+++ ems.api/Program.cs
@@ -129,8 +129,8 @@ try
     app.UseMiddleware<ErrorHandlerMiddleware>();
 
     app.MapControllers();
-     var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-    app.Urls.Add($"http://*:{port}");
+   //  var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+   // app.Urls.Add($"http://*:{port}");
 
     app.Run();
 }
